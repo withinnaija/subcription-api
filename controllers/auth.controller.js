ee0cf3ff63ed2fba9e78dad84dbc85d6567d09bf
withinnaija <withinnaija@gmail.com>
2026-01-08 01:02:17 +0100

    implement sign in function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,5 +44,34 @@ export const signUp = async (req, res, next) => {
     next(error);
   }
 };
-export const signIn = async (req, res, next) => {};
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error("user not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    const isPassswordVlid = await bcrypt.compare(password, user.password);
+    if (!isPassswordVlid) {
+      const error = new Error("invalid password");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRE_IN,
+    });
+    res.status(200).json({
+      sucess: true,
+      message: "user signed in succesfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 export const signOut = async (req, res, next) => {};
